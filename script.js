2d7f22a8c2a66fab6881f3154f842d24751cd2bf
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-18 15:44:00 +0530

    add logic of volume of sphere

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+  let radius=document.getElementById('radius').value;
+  console.log(radius);
+  let ans;
+  if(radius){
+  ans=(4*3.14*radius*radius*radius)/3;
+  }
+  let volume=document.getElementById('volume');
+//   console.log(volume);
+if(ans!=undefined){
+    volume.value=ans;
+}
+
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
